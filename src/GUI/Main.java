d6a25d4596ce4b01615dc40d7776ad44c1159bfe
Denise <aldeadenise@yahoo.com>
2022-01-12 20:45:34 +0200

    idk please help

--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -7,29 +7,28 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
     @Override
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader mainLoader = new FXMLLoader();
         mainLoader.setLocation(getClass().getResource("mainWindow.fxml"));
         Parent mainWindow = mainLoader.load();
-        MainWindowController mainWindowController = mainLoader.getController();
+        //MainWindowController mainWindowController = mainLoader.getController();
         primaryStage.setTitle("Toy Language Interpreter");
         primaryStage.setScene(new Scene(mainWindow, 620, 600));
         primaryStage.show();
 
-        FXMLLoader secondaryLoader = new FXMLLoader();
-        secondaryLoader.setLocation(getClass().getResource("secondaryWindow.fxml"));
-        Parent secondaryWindow = secondaryLoader.load();
-        SecondaryWindowController secondaryWindowController = secondaryLoader.getController();
-        secondaryWindowController.setMainWindowController(mainWindowController);
-        Stage secondaryStage = new Stage();
-        secondaryStage.setTitle("Example");
-        secondaryStage.setScene(new Scene(secondaryWindow, 500, 550));
-        secondaryStage.show();
+//        FXMLLoader secondaryLoader = new FXMLLoader();
+//        secondaryLoader.setLocation(getClass().getResource("secondaryWindow.fxml"));
+//        Parent secondaryWindow = secondaryLoader.load();
+//        SecondaryWindowController secondaryWindowController = secondaryLoader.getController();
+//        secondaryWindowController.setMainWindowController(mainWindowController);
+//        Stage secondaryStage = new Stage();
+//        secondaryStage.setTitle("Example");
+//        secondaryStage.setScene(new Scene(secondaryWindow, 500, 550));
+//        secondaryStage.show();
     }
 
     public static void main(String[] args) {
-        launch(args);
+        launch();
     }
 }
--- src/module-info.java
+++
@@ -1,6 +0,0 @@
-module A5 {
-    requires javafx.fxml;
-    requires javafx.controls;
-
-    opens GUI;
-}
